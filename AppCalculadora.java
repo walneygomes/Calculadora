3cb22f907cb75afbc2f3222b7a79b763a21b7267
Walney Negreiros Gomes <36850527+walneygomes@users.noreply.github.com>
2019-03-03 20:06:10 -0300

    Add files via upload

---
+++ AppCalculadora.java
@@ -0,0 +1,35 @@
+
+public class AppCalculadora {
+	public static void main(String[] args){
+	   System.out.print("DIGITE UM VALOR");
+	   Calculadora  c= new Calculadora();
+	   while(true){
+	   System.out.println( "    O RESULTANDO  " + c.divisao(9, 2));
+	  System.out.println( "    O RESULTANDO  " + c.divisao(9, 2));
+	  System.out.println( "    O RESULTANDO  " + c.divisao(9, 2));
+	  System.out.println( "    O RESULTANDO  " + c.divisao(9, 2));
+	  System.out.println( "    O RESULTANDO  " + c.divisao(9, 2));
+	  System.out.println( "    O RESULTANDO  " + c.divisao(9, 2));
+	  System.out.println( "    O RESULTANDO  " + c.divisao(9, 2));
+	  System.out.println( "    O RESULTANDO  " + c.divisao(9, 2));
+	  System.out.println( "    O RESULTANDO  " + c.divisao(9, 2));
+	  System.out.println( "    O RESULTANDO  " + c.divisao(9, 2));
+	  System.out.println( "    O RESULTANDO  " + c.divisao(9, 2));
+	  System.out.println( "    O RESULTANDO  " + c.divisao(9, 2));
+	  System.out.println( "    O RESULTANDO  " + c.divisao(9, 2));
+	  System.out.println( "    O RESULTANDO  " + c.divisao(9, 2));
+	  System.out.println( "    O RESULTANDO  " + c.divisao(9, 2));
+	  System.out.println( "    O RESULTANDO  " + c.divisao(9, 2));
+	  System.out.println( "    O RESULTANDO  " + c.divisao(9, 2));
+	  System.out.println( "    O RESULTANDO  " + c.divisao(9, 2));
+	  System.out.println( "    O RESULTANDO  " + c.divisao(9, 2));
+	   }
+
+	   
+		
+		
+		 
+	}
+
+}
+ 
---
+++ Calculadora.java
@@ -0,0 +1,33 @@
+
+public class Calculadora {
+	
+	public double potencia(double valor){
+        double potencia=valor*valor;
+		return potencia;
+		
+	}
+	public double divisao(double valor,double valor1){
+		return valor/valor1;
+	}
+
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+}
